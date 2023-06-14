2533d631d2444ea23df26267bcb587b1fa519422
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-14 14:48:17 +0200

    Added body style

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,10 @@ import AppTitle from './components/AppTitle.vue'
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+body {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
