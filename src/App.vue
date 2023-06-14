ab5d9cf882e0deb86b0ee117883134e501143ed0
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-14 16:31:18 +0200

    redefined apphero and styled jumbiotron

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-import AppTitle from './components/AppTitle.vue'
+import AppHero from './components/AppHero.vue'
 </script>
 
 <template>
-  <AppTitle />
+  <AppHero />
 </template>
 
 <style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,33 @@
+<script>
+    export default {
+      data() {
+        return {
+            imgHeader: {
+              imgSrc: "https://img.freepik.com/premium-photo/painting-mountain-landscape-with-river-foreground-mountain-background_864560-11.jpg",
+              imgAlt: "scenery",
+            },
+        }
+      },
+    }
+</script>
+
+<template>
+    <header class="jumbotron">
+        <img :src="imgHeader.imgSrc" :alt="imgHeader.imgAlt">
+    </header>
+</template>
+
+<style scoped>
+  .jumbotron {
+    width: 1200px;
+    height: 550px;
+    border-radius: 1rem;
+    overflow: hidden;
+    outline: 1px solid red;
+  }
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+</style>
