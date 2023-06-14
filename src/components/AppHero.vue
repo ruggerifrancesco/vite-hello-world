0b74c010a9e94fb827356b99540f7ddfda66c4ad
ruggerifrancesco <lavoro.francescoruggeri@gmail.com>
2023-06-14 16:32:01 +0200

    added position top to jumbotron

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -23,11 +23,11 @@
     height: 550px;
     border-radius: 1rem;
     overflow: hidden;
-    outline: 1px solid red;
   }
   img {
     width: 100%;
     height: 100%;
     object-fit: cover;
+    object-position: top;
   }
 </style>
